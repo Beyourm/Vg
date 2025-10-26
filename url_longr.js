27ccf4fc59b976a3191e039d5849a11485e0729d
Beyourm <almushkibdullah@gmail.com>
2025-10-27 02:24:21 +0300

    Create url_longr.js

---
+++ url_longr.js
@@ -0,0 +1,6 @@
+window.COURSES_API_URL = "https://script.google.com/macros/s/AKfycbxHyHcUY1hN5K8o4POBiiEZTw2MK72TNyLdNGqXcTsdJRQ5xUP3BJxjZUxAyS5lg9PZOA/exec";
+
+    // رابط إرسال البيانات
+    const SUBMISSION_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzP4ugT4iShAZ2IJ7HvfUzqmvl1wi5K7GNG2qsilc9GHi6N9GuTb6h1r0bo5ce9Wpr8/exec";
+    //رابط التحقق من كود المسوق
+    const VALIDATION_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyVaKjjFeQL2uy-6K_iCna6H1-nJuMnLjONJ8ZnEWDAb4B3GE15rY6PTj4Tb56GDBSCOw/exec";
